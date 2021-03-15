54f9b7bb310d05544a1372435bfc657fce56a8b5
Caitlyn <caitlynmiley.cm@gmail.com>
2021-03-14 23:29:33 -0500

    created my own cards. updated naming convention. added flexbox

--- src/Projects.js
+++ src/Projects.js
@@ -2,6 +2,9 @@ import React from 'react'
 import "./Projects.css";
 
 function Projects() {
+
+  
+
     return (
        
 
@@ -10,58 +13,28 @@ function Projects() {
             <h1 className="projects__Text">
               Projects
             </h1>
-        
-     
-    
-      
-        
-       
-      <div className="card__Row">
-        <div className="card__Column">
-          <div className="card">
-            <img src="https://dummyimage.com/600x400/000/fff.jpg" className="card-img-top" alt="..."></img>
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            </div>
-          </div>
         </div>
-        <div className="card__Column">
-          <div className="card">
-            <img src="https://dummyimage.com/600x400/000/fff.jpg" className="card-img-top" alt="..."></img>
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            </div>
-          </div>
-        </div>
-        <div className="card__Column">
-          <div className="card">
-            <img src="https://dummyimage.com/600x400/000/fff.jpg" className="card-img-top" alt="..."></img>
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
-            </div>
-          </div>
-        </div>
-       
-        </div>
-      </div>
 
-      </div>
-        
-        
-          
-    
-        
-        
-    
+
+{/* start project card */}
+
+      
+
+
+
+<div class="card"></div>
+<div class="card card-1"></div>
+<div class="card card-2"></div>
+<div class="card card-3"></div>
+
+</div>
+                        
+
       
 
 
 
 
-    )
-}
+    )}
 
 export default Projects
